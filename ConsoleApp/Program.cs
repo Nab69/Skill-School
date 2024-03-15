a8fc802885a9a7c157db660ad8d40d1ad7205998
Nab69 <17692533+Nab69@users.noreply.github.com>
2024-03-15 11:20:12 +0100

    step 4

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using Dal;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestDb();
+            Console.WriteLine("Hello, World!");
+        }
+
+        static void TestDb()
+        {
+            using (var context = new SchoolContext())
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                context.People.ToList();
+
+            }// appel Dispose() implicite
+        }
+    }
+}
